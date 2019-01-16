f2847a985578c449155767b77cf45b46a07fb86e
William Durand <will+git@drnd.me>
2019-01-16 10:57:54 +0100

    Configure CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
 import logo from './logo.svg';
-import './App.scss';
+import styles from './App.module.scss';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={styles.container}>
+        <header className={styles.header}>
+          <img src={logo} className={styles.logo} alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-            className="App-link"
+            className={styles.link}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
