bbe3736e428e8e4a708c8ed785cb85fd76d0abcb
Bob Silverberg <bob.silverberg@gmail.com>
2019-01-16 15:15:52 -0500

    Configure React Router for Create React App

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {HashRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+ReactDOM.render((
+  <HashRouter>
+    <App />
+  </HashRouter>
+), document.getElementById('root') as HTMLElement);
