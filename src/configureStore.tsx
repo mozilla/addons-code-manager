938ac759ba4279eb8b1f1a06ff9b7985408af9af
Kumar McMillan <kumar.mcmillan@gmail.com>
2019-01-23 14:40:27 -0600

    Tighten up type safety in configureStore

--- src/configureStore.tsx
+++ src/configureStore.tsx
@@ -5,28 +5,26 @@ import { createLogger } from 'redux-logger';
 
 import example, { ExampleState } from './reducers/example';
 
-export interface ApplicationState {
-  example: ExampleState;
-}
-
 export interface ConnectedReduxProps<A extends Action = AnyAction> {
   dispatch: Dispatch<A>;
 }
 
-const reducers = { example };
+export interface ApplicationState {
+  example: ExampleState;
+}
+
+function createRootReducer() {
+  return combineReducers<ApplicationState>({ example });
+}
 
 export default function configureStore(
   preloadedState?: ApplicationState,
 ): Store<ApplicationState> {
-  const rootReducer = combineReducers<ApplicationState>(reducers);
-
   let middleware = applyMiddleware(createLogger());
   if (process.env.NODE_ENV !== 'production') {
     const composeEnhancers = composeWithDevTools({});
     middleware = composeEnhancers(middleware);
   }
 
-  const store = createStore(rootReducer, preloadedState, middleware);
-
-  return store;
+  return createStore(createRootReducer(), preloadedState, middleware);
 }
