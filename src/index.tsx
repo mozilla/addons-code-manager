4f71715e870600b220c19295189739140ff544a4
Bob Silverberg <bob.silverberg@gmail.com>
2019-01-29 09:19:47 -0500

    Address review comments

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import './index.css';
-import './custom.scss';
+import './index.scss';
 import App from './components/App';
 import configureStore from './configureStore';
 import { actions as apiActions } from './reducers/api';
