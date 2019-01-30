a9a9c056e8b9943f7d842aa3ba2418770e1b5e1f
Bob Silverberg <bob.silverberg@gmail.com>
2019-01-30 17:24:33 -0500

    Add a test for a diff with multiple files (#90)

--- src/components/DiffView/index.spec.tsx
+++ src/components/DiffView/index.spec.tsx
@@ -19,6 +19,26 @@ index 5ca1a30..4e2c90f 100644
 +  expect(root).toHaveProp('viewType', viewType);
  });`;
 
+  const multipleDiff = `diff --git a/src/index1.js b/src/index1.js
+index e69de29..d00491f 100644
+--- a/src/index1.js
++++ b/src/index1.js
+@@ -0,0 +1 @@
++1
+diff --git a/src/index.js b/src/index2.js
+index d00491f..0cfbf08 100644
+--- a/src/index2.js
++++ b/src/index2.js
+@@ -1 +1 @@
+-1
++2
+diff --git a/src/index3.js b/src/index3.js
+index 0cfbf08..e69de29 100644
+--- a/src/index3.js
++++ b/src/index3.js
+@@ -1 +0,0 @@
+-2`;
+
   const render = (props = {}) => {
     return shallow(<DiffView diff={basicDiff} {...props} />);
   };
@@ -42,5 +62,16 @@ index 5ca1a30..4e2c90f 100644
 
     expect(root.find(Diff)).toHaveProp('diffType', parsedDiff.type);
     expect(root.find(Diff)).toHaveProp('hunks', parsedDiff.hunks);
+  });
+
+  it('creates multiple Diff instances when there are multiple files in the diff', () => {
+    const parsedDiff = parseDiff(multipleDiff);
+    const root = shallow(<DiffView diff={multipleDiff} />);
+
+    expect(root.find(Diff)).toHaveLength(parsedDiff.length);
+    parsedDiff.forEach((diff, index) => {
+      expect(root.find(Diff).at(index)).toHaveProp('diffType', diff.type);
+      expect(root.find(Diff).at(index)).toHaveProp('hunks', diff.hunks);
+    });
   });
 });
