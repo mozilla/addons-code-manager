a900ade2dcbb456de20646c14068c83fcf0bfdc8
William Durand <will+git@drnd.me>
2019-01-31 10:33:53 +0100

    Add tests for deps (#83)

---
+++ src/packageJson.spec.tsx
@@ -0,0 +1,33 @@
+import packageJson from '../package.json';
+
+describe(__filename, () => {
+  const skipDevDeps = [
+    // There is a conflict between CRA and Storybook, see:
+    // https://github.com/mozilla/addons-code-manager/issues/91
+    'babel-loader',
+    // Prettier recommends to pin the version to avoid unreviewed changes.
+    'prettier',
+  ];
+
+  it.each(Object.keys(packageJson.dependencies))(
+    `should have dependencies[%s] version prefixed with a number`,
+    (key: string) => {
+      expect(packageJson.dependencies).toHaveProperty(
+        key,
+        expect.stringMatching(/^\d/),
+      );
+    },
+  );
+
+  it.each(Object.keys(packageJson.devDependencies))(
+    `should have devDependencies[%s] version prefixed with "^"`,
+    (key: string) => {
+      if (!skipDevDeps.includes(key)) {
+        expect(packageJson.devDependencies).toHaveProperty(
+          key,
+          expect.stringMatching(/^(\^|git)/),
+        );
+      }
+    },
+  );
+});
