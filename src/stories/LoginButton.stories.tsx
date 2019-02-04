e3730617d8d1436b578c6ef8b7aae4b79e382b17
Kumar McMillan <kumar.mcmillan@gmail.com>
2019-02-04 10:26:01 -0600

    code cleanup

--- src/stories/LoginButton.stories.tsx
+++ src/stories/LoginButton.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+
 import LoginButton from '../components/LoginButton';
 
-storiesOf('LoginButton', module)
-  .add('not logged in', () => <LoginButton />);
+storiesOf('LoginButton', module).add('not logged in', () => <LoginButton />);
--- src/stories/setup/config.ts
+++ src/stories/setup/config.ts
@@ -1,9 +1,7 @@
 import { configure } from '@storybook/react';
+
 // Automatically import all files ending in *.stories.tsx
 const req = require.context('../', true, /.stories.tsx$/);
 
-function loadStories() {
-  req.keys().forEach(req);
-}
-
+const loadStories = () => req.keys().forEach(req);
 configure(loadStories, module);
