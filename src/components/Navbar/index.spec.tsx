4738f7bd197706bfb9e45e18142d85f7ee6ae371
Bob Silverberg <bob.silverberg@gmail.com>
2019-02-05 13:40:03 -0500

    Update test

--- src/components/Navbar/index.spec.tsx
+++ src/components/Navbar/index.spec.tsx
@@ -9,7 +9,7 @@ import { fakeUser } from '../../test-helpers';
 import { logOutFromServer } from '../../api';
 import { actions as userActions } from '../../reducers/users';
 
-import Navbar from '.';
+import Navbar, { NavbarBase } from '.';
 
 describe(__filename, () => {
   type RenderParams = {
@@ -80,14 +80,25 @@ describe(__filename, () => {
   });
 
   describe('Log out button', () => {
-    it('calls logOutFromServer when clicked', () => {
+    it('configures the click handler', () => {
+      const root = render({ store: storeWithUser() });
+      const instance = root.instance() as NavbarBase;
+
+      expect(root.find(`.${styles.logOut}`)).toHaveProp(
+        'onClick',
+        instance.logOut,
+      );
+    });
+
+    it('calls logOutFromServer when clicked', async () => {
       const logOutFromServerMock = jest.fn();
       const root = render({
         _logOutFromServer: logOutFromServerMock,
         store: storeWithUser(),
       });
 
-      root.find(`.${styles.logOut}`).simulate('click');
+      const instance = root.instance() as NavbarBase;
+      await instance.logOut();
       expect(logOutFromServerMock).toHaveBeenCalled();
     });
 
@@ -97,7 +108,8 @@ describe(__filename, () => {
 
       const root = render({ store });
 
-      await root.find(`.${styles.logOut}`).simulate('click');
+      const instance = root.instance() as NavbarBase;
+      await instance.logOut();
       expect(dispatch).toHaveBeenCalledWith(userActions.logOut());
     });
   });
