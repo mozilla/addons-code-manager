625275dd6302ed8a81a1d45aaf77bdf6349a8e3a
William Durand <will+git@drnd.me>
2019-02-21 18:31:31 +0100

    Use Bootstrap's Col instead of div in Index component (#244)

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import { Col } from 'react-bootstrap';
 
 type PublicProps = {};
 
 export class IndexBase extends React.Component<PublicProps> {
   render() {
     return (
-      <div>
+      <Col>
         <p>
           There is nothing you can do here, but try{' '}
           <Link to="/en-US/browse/502955/versions/1541798/">
             browsing this add-on version.
           </Link>
         </p>
-      </div>
+      </Col>
     );
   }
 }
