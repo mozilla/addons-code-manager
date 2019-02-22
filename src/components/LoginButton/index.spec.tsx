dee8b8fa7ee83316feb71dee136e17064488b71f
William Durand <will+git@drnd.me>
2019-02-22 08:52:19 +0100

    Use REACT_APP_DEFAULT_API_VERSION in `LoginButton` component (#250)

--- src/components/LoginButton/index.spec.tsx
+++ src/components/LoginButton/index.spec.tsx
@@ -5,15 +5,18 @@ import { Button } from 'react-bootstrap';
 import LoginButton from '.';
 
 describe(__filename, () => {
-  it('renders a page', () => {
+  it('renders a login button', () => {
+    const apiVersion = 'api-version';
     const fxaConfig = 'some-fxa-config';
 
-    const root = shallow(<LoginButton fxaConfig={fxaConfig} />);
+    const root = shallow(
+      <LoginButton fxaConfig={fxaConfig} apiVersion={apiVersion} />,
+    );
 
     expect(root.find(Button)).toHaveLength(1);
     expect(root.find(Button)).toHaveProp(
       'href',
-      `/api/v4/accounts/login/start/?config=${fxaConfig}&to=/`,
+      `/api/${apiVersion}/accounts/login/start/?config=${fxaConfig}&to=/`,
     );
   });
 });
