484e248d859160c703d2d535ab47b2520c931da5
William Durand <will+git@drnd.me>
2019-02-25 22:21:58 +0100

    fixes

--- stories/utils.tsx
+++ stories/utils.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { MemoryRouter as Router } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import configureStore from '../src/configureStore';
@@ -10,7 +10,7 @@ export const renderWithStoreAndRouter = (
 ) => {
   return (
     <Provider store={store}>
-      <Router>{element}</Router>
+      <MemoryRouter>{element}</MemoryRouter>
     </Provider>
   );
 };
