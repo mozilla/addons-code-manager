82467610f24f757f73d2dae743b78b1306fa7134
William Durand <will+git@drnd.me>
2019-02-27 13:37:56 +0100

    Add comment

--- src/components/CodeView/text-language.js
+++ src/components/CodeView/text-language.js
@@ -1,4 +1,6 @@
 /* eslint amo/only-tsx-files: 0 */
+// This file contains the implementation of a 'text' language for
+// Prism/refractor.
 
 const text = (Prism) => {
   // eslint-disable-next-line no-param-reassign
