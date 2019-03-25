2f4aa381f8c3c9556ad206e07a18cb3e0594a5d9
William Durand <will+git@drnd.me>
2019-03-25 14:48:36 +0100

    Continue to the initial URL after login

--- src/components/LoginButton/index.spec.tsx
+++ src/components/LoginButton/index.spec.tsx
@@ -1,22 +1,34 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
 import { Button } from 'react-bootstrap';
 
-import LoginButton from '.';
+import {
+  createContextWithFakeRouter,
+  createFakeLocation,
+  shallowUntilTarget,
+} from '../../test-helpers';
+
+import LoginButton, { LoginButtonBase } from '.';
 
 describe(__filename, () => {
   it('renders a login button', () => {
     const apiVersion = 'api-version';
     const fxaConfig = 'some-fxa-config';
+    const pathname = '/en-US/browse/491343/versions/1527716/';
 
-    const root = shallow(
+    const root = shallowUntilTarget(
       <LoginButton fxaConfig={fxaConfig} apiVersion={apiVersion} />,
+      LoginButtonBase,
+      {
+        shallowOptions: createContextWithFakeRouter({
+          location: createFakeLocation({ pathname }),
+        }),
+      },
     );
 
     expect(root.find(Button)).toHaveLength(1);
     expect(root.find(Button)).toHaveProp(
       'href',
-      `/api/${apiVersion}/accounts/login/start/?config=${fxaConfig}&to=/`,
+      `/api/${apiVersion}/accounts/login/start/?config=${fxaConfig}&to=${pathname}`,
     );
   });
 });
--- stories/LoginButton.stories.tsx
+++ stories/LoginButton.stories.tsx
@@ -2,5 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import LoginButton from '../src/components/LoginButton';
+import { renderWithStoreAndRouter } from './utils';
 
-storiesOf('LoginButton', module).add('default', () => <LoginButton />);
+storiesOf('LoginButton', module).add('default', () =>
+  renderWithStoreAndRouter(<LoginButton />),
+);
