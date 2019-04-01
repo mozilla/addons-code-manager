164d265781da81e71aea109e55dc65881e19f111
William Durand <will+git@drnd.me>
2019-04-01 19:14:42 +0200

    Pass absolute url to api login (fxa) (#505)

--- src/components/LoginButton/index.spec.tsx
+++ src/components/LoginButton/index.spec.tsx
@@ -1,39 +1,34 @@
 import * as React from 'react';
 import { Button } from 'react-bootstrap';
+import { shallow } from 'enzyme';
 
 import { makeApiURL } from '../../api';
-import {
-  createContextWithFakeRouter,
-  createFakeLocation,
-  shallowUntilTarget,
-} from '../../test-helpers';
 
-import LoginButton, { LoginButtonBase } from '.';
+import LoginButton from '.';
 
 describe(__filename, () => {
-  const render = ({ fxaConfig = 'fxa', pathname = '/' } = {}) => {
-    return shallowUntilTarget(
-      <LoginButton fxaConfig={fxaConfig} />,
-      LoginButtonBase,
-      {
-        shallowOptions: createContextWithFakeRouter({
-          location: createFakeLocation({ pathname }),
-        }),
-      },
-    );
+  const render = ({ fxaConfig = 'fxa', _window = window } = {}) => {
+    return shallow(<LoginButton fxaConfig={fxaConfig} _window={_window} />);
   };
 
   it('renders a login button', () => {
     const fxaConfig = 'some-fxa-config';
-    const pathname = '/en-US/browse/491343/versions/1527716/';
+    const href = 'https://example.org/en-US/browse/4913/versions/1527/';
+    const _window = {
+      ...window,
+      location: {
+        ...window.location,
+        href,
+      },
+    };
 
-    const root = render({ fxaConfig, pathname });
+    const root = render({ fxaConfig, _window });
 
     expect(root.find(Button)).toHaveLength(1);
     expect(root.find(Button)).toHaveProp(
       'href',
       makeApiURL({
-        path: `/accounts/login/start/?config=${fxaConfig}&to=${pathname}`,
+        path: `/accounts/login/start/?config=${fxaConfig}&to=${href}`,
       }),
     );
   });
