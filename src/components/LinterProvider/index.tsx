e2fbeebe1553a0f7378a2eae8382e3f45583438a
Kumar McMillan <kumar.mcmillan@gmail.com>
2019-04-15 13:21:06 -0500

    Always re-render LinterProvider on location change (#584)

--- src/components/LinterProvider/index.tsx
+++ src/components/LinterProvider/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 
 import { ApplicationState } from '../../reducers';
 import { ConnectedReduxProps } from '../../configureStore';
@@ -37,7 +38,8 @@ type PropsFromState = LinterProviderInfo;
 export type Props = PublicProps &
   DefaultProps &
   PropsFromState &
-  ConnectedReduxProps;
+  ConnectedReduxProps &
+  RouteComponentProps;
 
 export class LinterProviderBase extends React.Component<Props> {
   loadData: LoadData;
@@ -93,7 +95,9 @@ export class LinterProviderBase extends React.Component<Props> {
 
 const mapStateToProps = (
   state: ApplicationState,
-  ownProps: PublicProps,
+  // Note that ownProps must also be defined here because of how we rely on
+  // ownProps.location via withRouter(). See the comment below for details.
+  ownProps: PublicProps & RouteComponentProps,
 ): PropsFromState => {
   const { selectedPath, versionId } = ownProps;
 
@@ -113,6 +117,8 @@ const mapStateToProps = (
   };
 };
 
-export default connect(mapStateToProps)(
-  LinterProviderBase,
-) as React.ComponentType<PublicProps & Partial<DefaultProps>>;
+// withRouter() is currently only used to control the memoization
+// behavior of mapStateToProps(). We need new values of ownProps.location
+// to trigger a re-render.
+// More details: https://github.com/mozilla/addons-code-manager/issues/559
+export default withRouter(connect(mapStateToProps)(LinterProviderBase));
