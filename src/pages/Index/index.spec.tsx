32a80f773a2da0e7349e4bcb4d05ef7004c53ed4
William Durand <will+git@drnd.me>
2019-04-25 16:01:41 +0200

    Update index page with some links (#614)

--- src/pages/Index/index.spec.tsx
+++ src/pages/Index/index.spec.tsx
@@ -5,8 +5,15 @@ import { Link } from 'react-router-dom';
 import Index from '.';
 
 describe(__filename, () => {
-  it('renders a page', () => {
+  it('renders a page with some links', () => {
     const root = shallow(<Index />);
+
+    expect(root.find('a')).toExist();
+    expect(root.find(Link)).not.toExist();
+  });
+
+  it('renders some example links for local dev', () => {
+    const root = shallow(<Index showLocalDevLinks />);
 
     expect(root.find(Link)).toExist();
   });
--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,27 +1,72 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 import { Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-type PublicProps = {};
+import { gettext } from '../../utils';
 
-export class IndexBase extends React.Component<PublicProps> {
+type Props = {
+  showLocalDevLinks: boolean;
+};
+
+export class IndexBase extends React.Component<Props> {
+  static defaultProps = {
+    showLocalDevLinks: process.env.REACT_APP_IS_LOCAL_DEV === 'true',
+  };
+
   render() {
+    const { showLocalDevLinks } = this.props;
+
+    const apiHost = process.env.REACT_APP_API_HOST;
+    const repoUrl = 'https://github.com/mozilla/addons-code-manager';
+
     return (
       <Col>
         <p>
-          There is nothing you can do here, but try{' '}
-          <Link to="/en-US/browse/494431/versions/1532144/">
-            browsing this add-on version
-          </Link>
-          {', '}
-          <Link to="/en-US/compare/502955/versions/1541794...1541798/">
-            look at this compare view
-          </Link>{' '}
-          or go to this{' '}
-          <Link to="/en-US/browse/502955/versions/1000000/">
-            page that will generate an error.
-          </Link>
+          {gettext(
+            'This is a tool for managing add-on source code that is used with the',
+          )}{' '}
+          <a href={`${apiHost}/reviewers/`}>{gettext('Reviewers Tools')}</a>.
         </p>
+        <p>
+          {gettext(`🚧 This project is under active development. If you find a
+            bug, please`)}{' '}
+          <a href={`${repoUrl}/issues/new`}>{gettext('file an issue')}</a>.
+        </p>
+        <p>{gettext('Other useful links:')}</p>
+        <ul>
+          <li>
+            <a href={apiHost}>{gettext('AMO (frontend)')}</a>
+          </li>
+          <li>
+            <a href={`${apiHost}/developers/`}>{gettext('Developer Hub')}</a>
+          </li>
+          <li>
+            <a href={repoUrl}>{gettext('GitHub repository')}</a>
+          </li>
+        </ul>
+        {showLocalDevLinks && (
+          <React.Fragment>
+            <hr />
+            <p>{gettext('Dev links (only shown in local dev):')}</p>
+            <ul>
+              <li>
+                <Link to="/en-US/browse/494431/versions/1532144/">
+                  a browse page
+                </Link>
+              </li>
+              <li>
+                <Link to="/en-US/compare/502955/versions/1541794...1541798/">
+                  a compare page
+                </Link>
+              </li>
+              <li>
+                <Link to="/en-US/browse/502955/versions/1000000/">
+                  a browse page that will generate an error
+                </Link>
+              </li>
+            </ul>
+          </React.Fragment>
+        )}
       </Col>
     );
   }
