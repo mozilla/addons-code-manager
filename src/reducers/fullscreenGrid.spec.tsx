4eef401a20f3d14bf3331f551cf481b2e34d4713
William Durand <will+git@drnd.me>
2019-05-13 09:42:27 +0200

    fixes

--- src/reducers/fullscreenGrid.spec.tsx
+++ src/reducers/fullscreenGrid.spec.tsx
@@ -1,6 +1,14 @@
-import reducer, { actions } from './fullscreenGrid';
+import reducer, { actions, initialState } from './fullscreenGrid';
 
 describe(__filename, () => {
+  it('is configured to expand the main side panel by default', () => {
+    expect(initialState.mainSidePanelIsExpanded).toEqual(true);
+  });
+
+  it('is configured to expand the alt side panel by default', () => {
+    expect(initialState.altSidePanelIsExpanded).toEqual(true);
+  });
+
   describe('toggleMainSidePanel', () => {
     it('collapses the main side panel by default', () => {
       const state = reducer(undefined, actions.toggleMainSidePanel());
@@ -15,6 +23,23 @@ describe(__filename, () => {
       state = reducer(state, actions.toggleMainSidePanel());
 
       expect(state.mainSidePanelIsExpanded).toEqual(true);
+    });
+  });
+
+  describe('toggleAltSidePanel', () => {
+    it('collapses the alt side panel by default', () => {
+      const state = reducer(undefined, actions.toggleAltSidePanel());
+
+      expect(state.altSidePanelIsExpanded).toEqual(false);
+    });
+
+    it('expands the alt side panel if collapsed', () => {
+      // Collapse the alt side panel
+      let state = reducer(undefined, actions.toggleAltSidePanel());
+      // Expand it
+      state = reducer(state, actions.toggleAltSidePanel());
+
+      expect(state.altSidePanelIsExpanded).toEqual(true);
     });
   });
 });
--- src/reducers/fullscreenGrid.tsx
+++ src/reducers/fullscreenGrid.tsx
@@ -2,14 +2,17 @@ import { Reducer } from 'redux';
 import { ActionType, createAction, getType } from 'typesafe-actions';
 
 export const actions = {
+  toggleAltSidePanel: createAction('TOGGLE_ALT_SIDE_PANEL'),
   toggleMainSidePanel: createAction('TOGGLE_MAIN_SIDE_PANEL'),
 };
 
 export type FullscreenGridState = {
+  altSidePanelIsExpanded: boolean;
   mainSidePanelIsExpanded: boolean;
 };
 
 export const initialState: FullscreenGridState = {
+  altSidePanelIsExpanded: true,
   mainSidePanelIsExpanded: true,
 };
 
@@ -18,6 +21,11 @@ const reducer: Reducer<FullscreenGridState, ActionType<typeof actions>> = (
   action,
 ): FullscreenGridState => {
   switch (action.type) {
+    case getType(actions.toggleAltSidePanel):
+      return {
+        ...state,
+        altSidePanelIsExpanded: !state.altSidePanelIsExpanded,
+      };
     case getType(actions.toggleMainSidePanel):
       return {
         ...state,
