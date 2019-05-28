675f6afd99db0784773c2c3ee9ec30c6442623c9
Kumar McMillan <kumar.mcmillan@gmail.com>
2019-05-28 12:04:04 -0500

    Send log.error() messages to Sentry (#785)

--- src/configureApplication.tsx
+++ src/configureApplication.tsx
@@ -1,5 +1,6 @@
+import * as Sentry from '@sentry/browser';
+import { CaptureConsole } from '@sentry/integrations';
 import log from 'loglevel';
-import Raven from 'raven-js';
 import { config, library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
@@ -13,31 +14,36 @@ export type ClientEnvVars = {
 };
 
 type ConfigureApplicationParams = {
-  _Raven?: typeof Raven;
+  _Sentry?: typeof Sentry;
   _log?: typeof log;
   env?: ClientEnvVars;
 };
 
 const configureApplication = ({
-  _Raven = Raven,
+  _Sentry = Sentry,
   _log = log,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   env = (process.env as any) as ClientEnvVars,
 }: ConfigureApplicationParams = {}) => {
+  if (env.REACT_APP_SENTRY_DSN) {
+    _Sentry.init({
+      dsn: env.REACT_APP_SENTRY_DSN,
+      // This must be configured before any calls to log.setLevel().
+      integrations: [new CaptureConsole({ levels: ['error'] })],
+    });
+    _Sentry.configureScope((scope) => {
+      // In case we send server errors in the future, this will allow us
+      // to filter out historic client errors.
+      scope.setTag('app_logger', 'client');
+    });
+  }
+
   if (env.NODE_ENV === 'production') {
     // The second parameter prevents the log level to be persisted in a cookie
     // or localStorage.
     _log.setLevel(log.levels.INFO, false);
   } else {
     _log.setLevel(log.levels.DEBUG, false);
-  }
-
-  if (env.REACT_APP_SENTRY_DSN) {
-    _Raven
-      .config(env.REACT_APP_SENTRY_DSN, {
-        logger: 'client',
-      })
-      .install();
   }
 
   // Turn off inline CSS injection for CSP. Must be before all other
