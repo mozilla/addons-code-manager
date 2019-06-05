47c40296d7fde63bce58ff0555e7bdb6b7b414af
mirefly <mirefly@gmail.com>
2019-06-05 03:55:42 -0600

    higher contrast ratio for text files (#807)

--- src/refractor/index.tsx
+++ src/refractor/index.tsx
@@ -2,6 +2,7 @@ import refractor from 'refractor';
 
 import text from './text-language';
 import './prism.css';
+import './prism.overrides.scss';
 
 // This is needed to provide a fallback in `getLanguage()` when the mime type
 // has no corresponding syntax highlighting language.
