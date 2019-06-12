3d5dc4ab3e2172aba735d6017e0a8868e75d3ea0
Kumar McMillan <kumar.mcmillan@gmail.com>
2019-06-12 10:54:24 -0500

    Fix the FullscreenGrid story (#832)

--- stories/FullscreenGrid.stories.tsx
+++ stories/FullscreenGrid.stories.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import ContentShell from '../src/components/FullscreenGrid/ContentShell';
-import FullscreenGrid, {
-  Header,
-  PanelAttribs,
-} from '../src/components/FullscreenGrid';
-import ToggleButton from '../src/components/ToggleButton';
+import FullscreenGrid, { Header } from '../src/components/FullscreenGrid';
 import {
   generateParagraphs,
   renderWithStoreAndRouter,
@@ -15,23 +11,35 @@ import {
 
 const getParams = () => rootAttributeParams({ fullscreen: true });
 
+const renderInGrid = (content: JSX.Element) => {
+  return renderWithStoreAndRouter(
+    <FullscreenGrid>
+      <Header className="FullscreenGridStory-Header">
+        <div className="FullscreenGridStory-placeholder">Header</div>
+      </Header>
+      {content}
+    </FullscreenGrid>,
+  );
+};
+
 storiesOf('FullscreenGrid', module)
   .add(
     'default',
     () => {
-      return renderWithStoreAndRouter(
-        <FullscreenGrid>
-          <Header className="FullscreenGridStory-Header">Header</Header>
-          <ContentShell
-            altSidePanel={PanelAttribs.altSidePanel}
-            altSidePanelClass="FullscreenGridStory-altSidePanel"
-            className="FullscreenGridStory-content"
-            mainSidePanel={PanelAttribs.mainSidePanel}
-            mainSidePanelClass="FullscreenGridStory-mainSidePanel"
-          >
-            Content
-          </ContentShell>
-        </FullscreenGrid>,
+      return renderInGrid(
+        <ContentShell
+          altSidePanel={
+            <div className="FullscreenGridStory-placeholder">altSidePanel</div>
+          }
+          altSidePanelClass="FullscreenGridStory-altSidePanel"
+          className="FullscreenGridStory-content"
+          mainSidePanel={
+            <div className="FullscreenGridStory-placeholder">mainSidePanel</div>
+          }
+          mainSidePanelClass="FullscreenGridStory-mainSidePanel"
+        >
+          <div className="FullscreenGridStory-placeholder">Content</div>
+        </ContentShell>,
       );
     },
     getParams(),
@@ -41,13 +49,10 @@ storiesOf('FullscreenGrid', module)
     () => {
       const someText = generateParagraphs(10);
 
-      return renderWithStoreAndRouter(
-        <FullscreenGrid>
-          <Header className="FullscreenGridStory-Header">Header</Header>
-          <ContentShell altSidePanel={someText} mainSidePanel={someText}>
-            {someText}
-          </ContentShell>
-        </FullscreenGrid>,
+      return renderInGrid(
+        <ContentShell altSidePanel={someText} mainSidePanel={someText}>
+          {someText}
+        </ContentShell>,
       );
     },
     getParams(),
