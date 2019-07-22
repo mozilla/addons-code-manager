e5b1b5b14ca7329d71a4efd53e531ef3b679dafc
Kumar McMillan <kumar.mcmillan@gmail.com>
2019-07-22 12:00:40 -0500

    Show an alert for slow loading files in CodeView

--- src/components/CodeView/index.tsx
+++ src/components/CodeView/index.tsx
@@ -3,6 +3,7 @@ import { withRouter, Link, RouteComponentProps } from 'react-router-dom';
 import makeClassName from 'classnames';
 
 import styles from './styles.module.scss';
+import FadableContent from '../FadableContent';
 import LinterMessage from '../LinterMessage';
 import {
   getCodeLineAnchor,
@@ -11,10 +12,18 @@ import {
   mapWithDepth,
 } from './utils';
 import refractor from '../../refractor';
-import { getLanguageFromMimeType } from '../../utils';
+import {
+  gettext,
+  getLanguageFromMimeType,
+  shouldAllowSlowPages,
+} from '../../utils';
 import { Version } from '../../reducers/versions';
 import LinterProvider, { LinterProviderInfo } from '../LinterProvider';
 import GlobalLinterMessages from '../GlobalLinterMessages';
+import SlowPageAlert from '../SlowPageAlert';
+
+// This is how many lines of code it takes to slow down the UI.
+const SLOW_LOADING_LINE_COUNT = 1000;
 
 // This function mimics what https://github.com/rexxars/react-refractor does,
 // but we need a different layout to inline comments so we cannot use this
@@ -46,13 +55,17 @@ export const scrollToSelectedLine = (
 };
 
 export type PublicProps = {
-  _scrollToSelectedLine?: typeof scrollToSelectedLine;
   mimeType: string;
   content: string;
   version: Version;
 };
 
-type Props = PublicProps & RouteComponentProps;
+export type DefaultProps = {
+  _scrollToSelectedLine: typeof scrollToSelectedLine;
+  _slowLoadingLineCount: number;
+};
+
+type Props = PublicProps & DefaultProps & RouteComponentProps;
 
 type RowProps = {
   className: string;
@@ -61,15 +74,46 @@ type RowProps = {
 };
 
 export class CodeViewBase extends React.Component<Props> {
+  static defaultProps: DefaultProps = {
+    _scrollToSelectedLine: scrollToSelectedLine,
+    _slowLoadingLineCount: SLOW_LOADING_LINE_COUNT,
+  };
+
   renderWithLinterInfo = ({ selectedMessageMap }: LinterProviderInfo) => {
     const {
-      _scrollToSelectedLine = scrollToSelectedLine,
+      _scrollToSelectedLine,
+      _slowLoadingLineCount,
       content,
       location,
       mimeType,
     } = this.props;
 
     const language = getLanguageFromMimeType(mimeType);
+    let codeLines = getLines(content);
+    let codeWasTrimmed = false;
+    let codeIsSlowAlert;
+
+    if (codeLines.length >= _slowLoadingLineCount) {
+      if (!shouldAllowSlowPages(location)) {
+        codeLines = codeLines.slice(0, _slowLoadingLineCount);
+        codeWasTrimmed = true;
+      }
+      codeIsSlowAlert = (
+        <SlowPageAlert
+          location={location}
+          getMessage={(allowSlowPages: boolean) => {
+            return allowSlowPages
+              ? gettext('This file is loading slowly.')
+              : gettext('This file has been shortened to load faster.');
+          }}
+          getLinkText={(allowSlowPages: boolean) => {
+            return allowSlowPages
+              ? gettext('View a shortened file.')
+              : gettext('View the original file.');
+          }}
+        />
+      );
+    }
 
     return (
       <React.Fragment>
@@ -82,69 +126,75 @@ export class CodeViewBase extends React.Component<Props> {
           messages={selectedMessageMap && selectedMessageMap.global}
         />
 
-        <div className={styles.CodeView}>
-          <table className={styles.table}>
-            <tbody className={styles.tableBody}>
-              {getLines(content).map((code, i) => {
-                const line = i + 1;
+        {codeIsSlowAlert}
 
-                let rowProps: RowProps = {
-                  id: getCodeLineAnchorID(line),
-                  className: styles.line,
-                };
+        <FadableContent fade={codeWasTrimmed}>
+          <div className={styles.CodeView}>
+            <table className={styles.table}>
+              <tbody className={styles.tableBody}>
+                {codeLines.map((code, i) => {
+                  const line = i + 1;
 
-                if (isLineSelected(rowProps.id, location)) {
-                  rowProps = {
-                    ...rowProps,
-                    className: makeClassName(
-                      rowProps.className,
-                      styles.selectedLine,
-                    ),
-                    ref: _scrollToSelectedLine,
+                  let rowProps: RowProps = {
+                    id: getCodeLineAnchorID(line),
+                    className: styles.line,
                   };
-                }
 
-                return (
-                  <React.Fragment key={`fragment-${line}`}>
-                    <tr {...rowProps}>
-                      <td className={styles.lineNumber}>
-                        <Link
-                          to={{
-                            ...location,
-                            hash: getCodeLineAnchor(line),
-                          }}
-                        >{`${line}`}</Link>
-                      </td>
+                  if (isLineSelected(rowProps.id, location)) {
+                    rowProps = {
+                      ...rowProps,
+                      className: makeClassName(
+                        rowProps.className,
+                        styles.selectedLine,
+                      ),
+                      ref: _scrollToSelectedLine,
+                    };
+                  }
 
-                      <td className={styles.code}>
-                        {renderHighlightedCode(code, language)}
-                      </td>
-                    </tr>
-                    {selectedMessageMap && selectedMessageMap.byLine[line] && (
-                      <tr>
-                        <td
-                          id={`line-${line}-messages`}
-                          className={styles.linterMessages}
-                          colSpan={2}
-                        >
-                          {selectedMessageMap.byLine[line].map((msg) => {
-                            return (
-                              <LinterMessage
-                                inline
-                                key={msg.uid}
-                                message={msg}
-                              />
-                            );
-                          })}
+                  return (
+                    <React.Fragment key={`fragment-${line}`}>
+                      <tr {...rowProps}>
+                        <td className={styles.lineNumber}>
+                          <Link
+                            to={{
+                              ...location,
+                              hash: getCodeLineAnchor(line),
+                            }}
+                          >{`${line}`}</Link>
+                        </td>
+
+                        <td className={styles.code}>
+                          {renderHighlightedCode(code, language)}
                         </td>
                       </tr>
-                    )}
-                  </React.Fragment>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+                      {selectedMessageMap && selectedMessageMap.byLine[line] && (
+                        <tr>
+                          <td
+                            id={`line-${line}-messages`}
+                            className={styles.linterMessages}
+                            colSpan={2}
+                          >
+                            {selectedMessageMap.byLine[line].map((msg) => {
+                              return (
+                                <LinterMessage
+                                  inline
+                                  key={msg.uid}
+                                  message={msg}
+                                />
+                              );
+                            })}
+                          </td>
+                        </tr>
+                      )}
+                    </React.Fragment>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </FadableContent>
+        {/* Only show a slow alert at the bottom if the code was trimmed. */}
+        {codeWasTrimmed && codeIsSlowAlert}
       </React.Fragment>
     );
   };
@@ -164,4 +214,6 @@ export class CodeViewBase extends React.Component<Props> {
   }
 }
 
-export default withRouter(CodeViewBase);
+export default withRouter(CodeViewBase) as React.ComponentType<
+  PublicProps & Partial<DefaultProps>
+>;
