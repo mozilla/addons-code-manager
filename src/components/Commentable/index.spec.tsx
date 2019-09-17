49a962fcf9a06b425c3478d7e280dbe9caf0e345
Kumar McMillan <kumar.mcmillan@gmail.com>
2019-09-17 16:07:10 -0500

    Add features to Commentable needed by CodeView (#1060)

--- src/components/Commentable/index.spec.tsx
+++ src/components/Commentable/index.spec.tsx
@@ -1,13 +1,36 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
+import { Store } from 'redux';
 
+import configureStore from '../../configureStore';
+import AddComment from './AddComment';
 import styles from './styles.module.scss';
 
 import Commentable, { PublicProps } from '.';
 
 describe(__filename, () => {
+  const getRenderProps = ({
+    as = 'span',
+    children = () => <div />,
+    fileName = null,
+    line = null,
+    versionId = 1,
+    ...moreProps
+  }: Partial<PublicProps> = {}) => {
+    return { as, children, fileName, line, versionId, ...moreProps };
+  };
+
   const render = (moreProps: Partial<PublicProps> = {}) => {
-    return shallow(<Commentable as="span" {...moreProps} />);
+    return shallow(<Commentable {...getRenderProps(moreProps)} />);
+  };
+
+  const renderWithMount = ({
+    store = configureStore(),
+    ...moreProps
+  }: { store?: Store } & Partial<PublicProps> = {}) => {
+    return mount(<Commentable {...getRenderProps(moreProps)} />, {
+      context: { store },
+    });
   };
 
   it('lets you configure the custom element', () => {
@@ -25,14 +48,50 @@ describe(__filename, () => {
   it('is classified as commentable', () => {
     const root = render();
 
+    // This is important because it is instantiating a custom component.
     expect(root).toHaveClassName(styles.commentable);
   });
 
   it('renders children', () => {
     const className = 'child-class';
-    const children = <div className={className} />;
-    const root = render({ children });
+    const root = render({
+      children: (addComment) => (
+        <>
+          {addComment}
+          <div className={className} />
+        </>
+      ),
+    });
 
     expect(root.find(`.${className}`)).toHaveLength(1);
+  });
+
+  it('renders AddComment', () => {
+    const addCommentClassName = 'ExampleClass';
+    const fileName = 'manifest.json';
+    const line = 432;
+    const versionId = 1;
+    const root = render({
+      addCommentClassName,
+      children: (addComment) => addComment,
+      fileName,
+      line,
+      versionId,
+    });
+
+    const button = root.find(AddComment);
+    expect(button).toHaveProp('fileName', fileName);
+    expect(button).toHaveProp('line', line);
+    expect(button).toHaveProp('versionId', versionId);
+    expect(button).toHaveClassName(addCommentClassName);
+  });
+
+  it('can add a ref to the outer DOM node', () => {
+    const shellRef = jest.fn();
+
+    // We need `mount()` because `ref` is only used in a DOM environment.
+    const root = renderWithMount({ shellRef });
+
+    expect(shellRef).toHaveBeenCalledWith(root.getDOMNode());
   });
 });
--- src/components/Commentable/index.tsx
+++ src/components/Commentable/index.tsx
@@ -1,29 +1,45 @@
 import * as React from 'react';
 import makeClassName from 'classnames';
 
+import AddComment, { PublicProps as AddCommentProps } from './AddComment';
 import { AnyReactNode } from '../../typeUtils';
 import styles from './styles.module.scss';
 
 export type PublicProps = {
+  addCommentClassName?: string;
   as: React.ReactType;
-  children?: AnyReactNode;
+  children: (addComment: JSX.Element) => AnyReactNode;
   className?: string;
   id?: string;
-};
+  shellRef?: (element: HTMLElement | null) => void;
+} & AddCommentProps;
 
 // This represents something that can be commented on, such as a line of code.
 const CommentableBase = ({
+  addCommentClassName,
   as: AsComponent,
   children,
   className,
+  fileName,
   id,
+  line,
+  shellRef,
+  versionId,
 }: PublicProps) => {
   return (
     <AsComponent
       className={makeClassName(styles.commentable, className)}
       id={id}
+      ref={shellRef}
     >
-      {children}
+      {children(
+        <AddComment
+          className={addCommentClassName}
+          versionId={versionId}
+          fileName={fileName}
+          line={line}
+        />,
+      )}
     </AsComponent>
   );
 };
