575220a24a9a38155612d0553f5b8933b3de3f2d
Kumar McMillan <kumar.mcmillan@gmail.com>
2019-10-28 17:10:07 -0500

    Add Navbar actions to summarize all comments (#1170)

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,8 +3,10 @@ import { Button, Navbar } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
 import { gettext, getLocalizedString } from '../../utils';
+import CommentSummaryButton from '../CommentSummaryButton';
 import LoginButton from '../LoginButton';
 import { ApplicationState } from '../../reducers';
+import { Comment, selectVersionComments } from '../../reducers/comments';
 import { Version, selectCurrentVersionInfo } from '../../reducers/versions';
 import { ConnectedReduxProps } from '../../configureStore';
 import { User, selectCurrentUser, requestLogOut } from '../../reducers/users';
@@ -15,6 +17,7 @@ type PublicProps = {
 };
 
 type PropsFromState = {
+  comments: Comment[] | undefined;
   user: User | null;
   currentVersion: Version | null | undefined | false;
 };
@@ -32,17 +35,40 @@ export class NavbarBase extends React.Component<Props> {
     dispatch(_requestLogOut());
   };
 
+  renderCommentsNavBar() {
+    const { comments } = this.props;
+
+    if (!comments || comments.length === 0) {
+      return null;
+    }
+
+    return (
+      <>
+        <div className={styles.infoItem}>
+          {gettext('Comments:')}
+          <div className={styles.commentCount}>{comments.length}</div>
+        </div>
+        <div className={styles.infoItem}>
+          <CommentSummaryButton />
+        </div>
+      </>
+    );
+  }
+
   render() {
     const { user, currentVersion } = this.props;
 
     return (
-      <Navbar bg="dark" className={styles.Navbar} expand="lg" variant="dark">
+      <Navbar className={styles.Navbar} expand="lg" variant="dark">
         <Navbar.Brand className={styles.brand}>
-          {currentVersion && (
-            <span className={styles.addonName}>
-              {getLocalizedString(currentVersion.addon.name)}
-            </span>
-          )}
+          <div className={styles.info}>
+            {currentVersion && (
+              <div className={styles.infoItem}>
+                {getLocalizedString(currentVersion.addon.name)}
+              </div>
+            )}
+            {this.renderCommentsNavBar()}
+          </div>
         </Navbar.Brand>
         <Navbar.Text className={styles.text}>
           {user ? <span className={styles.username}>{user.name}</span> : null}
@@ -60,9 +86,16 @@ export class NavbarBase extends React.Component<Props> {
 }
 
 const mapStateToProps = (state: ApplicationState): PropsFromState => {
+  const currentVersion = selectCurrentVersionInfo(state.versions);
   return {
+    comments: currentVersion
+      ? selectVersionComments({
+          comments: state.comments,
+          versionId: currentVersion.id,
+        })
+      : undefined,
     user: selectCurrentUser(state.users),
-    currentVersion: selectCurrentVersionInfo(state.versions),
+    currentVersion,
   };
 };
 
--- stories/Navbar.stories.tsx
+++ stories/Navbar.stories.tsx
@@ -4,27 +4,50 @@ import { storiesOf } from '@storybook/react';
 import Navbar from '../src/components/Navbar';
 import configureStore from '../src/configureStore';
 import { actions as userActions } from '../src/reducers/users';
-import { fakeUser } from '../src/test-helpers';
+import {
+  createFakeExternalComment,
+  createStoreWithVersion,
+  createStoreWithVersionComments,
+  fakeUser,
+  fakeVersion,
+  fakeVersionAddon,
+} from '../src/test-helpers';
 import { renderWithStoreAndRouter } from './utils';
 
-storiesOf('Navbar', module).addWithChapters('all variants', {
-  chapters: [
-    {
-      sections: [
-        {
-          title: 'user is not signed in',
-          sectionFn: () => renderWithStoreAndRouter(<Navbar />),
-        },
-        {
-          title: 'user is signed in',
-          sectionFn: () => {
-            const store = configureStore();
-            store.dispatch(userActions.loadCurrentUser({ user: fakeUser }));
+const render = ({ store = configureStore(), ...props } = {}) => {
+  return renderWithStoreAndRouter(<Navbar {...props} />, { store });
+};
 
-            return renderWithStoreAndRouter(<Navbar />, { store });
-          },
-        },
+storiesOf('Navbar', module)
+  .add('user is not signed in', () => render())
+  .add('user is signed in', () => {
+    const store = configureStore();
+    store.dispatch(userActions.loadCurrentUser({ user: fakeUser }));
+
+    return render({ store });
+  })
+  .add('with current version', () => {
+    const version = {
+      ...fakeVersion,
+      addon: { ...fakeVersionAddon, name: { 'en-US': 'uBlock Origin' } },
+    };
+    const store = createStoreWithVersion({ version, makeCurrent: true });
+    return render({ store });
+  })
+  .add('user has entered comments', () => {
+    const store = createStoreWithVersionComments({
+      comments: [
+        createFakeExternalComment({
+          filename: 'manifest.json',
+          lineno: 23,
+          comment: 'This permission does not seem to be used anywhere. Is it?',
+        }),
+        createFakeExternalComment({
+          filename: 'background.js',
+          lineno: 344,
+          comment: 'This call to eval() is forbidden.',
+        }),
       ],
-    },
-  ],
-});
+    });
+    return render({ store });
+  });
