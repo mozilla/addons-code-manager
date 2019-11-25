5347990f20bcf4e9922c7682345e2839b798a7b3
William Durand <will+git@drnd.me>
2019-11-25 11:46:42 +0100

    prettier

--- src/jest-matchers/index.tsx
+++ src/jest-matchers/index.tsx
@@ -4,9 +4,7 @@ declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace, no-redeclare
   namespace jest {
     interface Matchers<R, T> {
-      urlWithTheseParams(params: {
-        [key: string]: string | undefined;
-      }): R;
+      urlWithTheseParams(params: { [key: string]: string | undefined }): R;
     }
 
     interface Expect {
