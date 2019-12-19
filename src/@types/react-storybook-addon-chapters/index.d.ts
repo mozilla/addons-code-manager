dbc70c7f85a8bb2afccaa9030786785f8c3c680d
William Durand <will+git@drnd.me>
2019-12-19 14:37:55 +0100

    Fix storybook-addon-chapters type
    I followed this approach: https://github.com/DefinitelyTyped/DefinitelyTyped/blob/06bef93b1891253d3923087ce5ac42eae67e3a26/types/storybook-addon-jsx/index.d.ts

--- src/@types/react-storybook-addon-chapters/index.d.ts
+++ src/@types/react-storybook-addon-chapters/index.d.ts
@@ -6,8 +6,13 @@ declare module 'react-storybook-addon-chapters' {
 }
 
 // This is used to expose the `addWithChapters` method.
-declare module '@storybook/react' {
-  export interface Story {
-    addWithChapters(title: string, config: object): this;
+declare module '@storybook/addons' {
+  interface ClientStoryApi<StoryFnReturnType = unknown> {
+    storiesOf(
+      kind: string,
+      module: NodeModule
+    ): StoryApi<StoryFnReturnType> & {
+      addWithChapters(title: string, config: object): this;
+    };
   }
 }
