dc1f2bcf95c01d7c358aac8cea42f58a0fdf97bc
William Durand <will+git@drnd.me>
2019-12-19 15:48:26 +0100

    fix type

--- src/@types/react-storybook-addon-chapters/index.d.ts
+++ src/@types/react-storybook-addon-chapters/index.d.ts
@@ -1,8 +1,5 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { Story } from '@storybook/react';
-
 declare module 'react-storybook-addon-chapters' {
-  const setDefaults = (options: object): void => {};
+  function setDefaults(options: object): void;
 }
 
 // This is used to expose the `addWithChapters` method.
@@ -10,7 +7,7 @@ declare module '@storybook/addons' {
   interface ClientStoryApi<StoryFnReturnType = unknown> {
     storiesOf(
       kind: string,
-      module: NodeModule
+      module: NodeModule,
     ): StoryApi<StoryFnReturnType> & {
       addWithChapters(title: string, config: object): this;
     };
