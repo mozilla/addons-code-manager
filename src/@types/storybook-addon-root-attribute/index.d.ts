3ac1ef5813dafc0e66745bd8c1d757e4cc79be8d
Mathieu Pillard <mpillard@mozilla.com>
2023-02-16 18:02:48 +0100

    (Temporarily ?) remove storybook to see the more important errors left

--- src/@types/storybook-addon-root-attribute/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'storybook-addon-root-attribute' {
-  import { StoryDecorator } from '@storybook/react';
-
-  declare function withRootAttribute(
-    ...params: Parameters<StoryDecorator>
-  ): ReturnValue<StoryDecorator>;
-}
